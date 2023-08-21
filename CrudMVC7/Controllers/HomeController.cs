9c2912a535b5a442b2923c4e51202f6ab98999d3
Cristian Pozo <cristian.pozo.ancho@gmail.com>
2023-08-21 17:47:17 -0500

    Add Contacts to show them in the view

--- CrudMVC7/Controllers/HomeController.cs
+++ CrudMVC7/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using CrudMVC7.Models;
+﻿using CrudMVC7.Data;
+using CrudMVC7.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CrudMVC7.Controllers
@@ -8,14 +10,19 @@ namespace CrudMVC7.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        // _context is the access to the database
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _context.Contacts.ToListAsync());
         }
 
         public IActionResult Privacy()
